54697302ad4139a8766b35e868a1cf4f575f45bd
Utkarsh <utkarsh20.ugcs32@iiitranchi.ac.in>
2024-03-15 23:08:20 +0530

    first commit

---
+++ frontend/src/pages/admin/studentRelated/ViewStudent.js
@@ -0,0 +1,256 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteUser, getUserDetails, updateUser } from '../../../redux/userRelated/userHandle';
+import { useNavigate, useParams } from 'react-router-dom';
+import { getSubjectList } from '../../../redux/sclassRelated/sclassHandle';
+import { Box, Button, IconButton, Table, TableBody, TableHead, Typography, Tab, Paper, BottomNavigation, BottomNavigationAction, Container } from '@mui/material';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import { Delete as DeleteIcon } from '@mui/icons-material';
+import { removeStuff, updateStudentFields } from '../../../redux/studentRelated/studentHandle';
+import CustomBarChart from '../../../components/CustomBarChart';
+import CustomPieChart from '../../../components/CustomPieChart';
+import { StyledTableCell, StyledTableRow } from '../../../components/styles';
+
+import InsertChartIcon from '@mui/icons-material/InsertChart';
+import InsertChartOutlinedIcon from '@mui/icons-material/InsertChartOutlined';
+import TableChartIcon from '@mui/icons-material/TableChart';
+import TableChartOutlinedIcon from '@mui/icons-material/TableChartOutlined';
+import Popup from '../../../components/Popup';
+
+const ViewStudent = () => {
+    const [showTab, setShowTab] = useState(false);
+    const [openStates, setOpenStates] = useState({});
+
+    const navigate = useNavigate()
+    const params = useParams()
+    const dispatch = useDispatch()
+    const { userDetails, response, loading, error } = useSelector((state) => state.user);
+
+    const studentID = params.id
+    const address = "Student"
+
+    useEffect(() => {
+        dispatch(getUserDetails(studentID, address));
+    }, [dispatch, studentID])
+
+    useEffect(() => {
+        if (userDetails && userDetails.sclassName && userDetails.sclassName._id !== undefined) {
+            dispatch(getSubjectList(userDetails.sclassName._id, "ClassSubjects"));
+        }
+    }, [dispatch, userDetails]);
+
+    if (response) { console.log(response) }
+    else if (error) { console.log(error) }
+
+    const [name, setName] = useState('');
+    const [rollNum, setRollNum] = useState('');
+    const [password, setPassword] = useState('');
+    const [sclassName, setSclassName] = useState('');
+    const [studentSchool, setStudentSchool] = useState('');
+    const [subjectMarks, setSubjectMarks] = useState('');
+
+    const [showPopup, setShowPopup] = useState(false);
+    const [message, setMessage] = useState("");
+
+    const handleOpen = (subId) => {
+        setOpenStates((prevState) => ({
+            ...prevState,
+            [subId]: !prevState[subId],
+        }));
+    };
+
+    const [value, setValue] = useState('1');
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    const [selectedSection, setSelectedSection] = useState('table');
+    const handleSectionChange = (event, newSection) => {
+        setSelectedSection(newSection);
+    };
+
+    const fields = password === ""
+        ? { name, rollNum }
+        : { name, rollNum, password }
+
+    useEffect(() => {
+        if (userDetails) {
+            setName(userDetails.name || '');
+            setRollNum(userDetails.rollNum || '');
+            setSclassName(userDetails.sclassName || '');
+            setStudentSchool(userDetails.school || '');
+            setSubjectMarks(userDetails.examResult || '');
+        }
+    }, [userDetails]);
+
+    const submitHandler = (event) => {
+        event.preventDefault()
+        dispatch(updateUser(fields, studentID, address))
+            .then(() => {
+                dispatch(getUserDetails(studentID, address));
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    }
+
+    const deleteHandler = () => {
+        setMessage("Sorry the delete function has been disabled for now.")
+        setShowPopup(true)
+
+        // dispatch(deleteUser(studentID, address))
+        //     .then(() => {
+        //         navigate(-1)
+        //     })
+    }
+
+    const removeHandler = (id, deladdress) => {
+        dispatch(removeStuff(id, deladdress))
+            .then(() => {
+                dispatch(getUserDetails(studentID, address));
+            })
+    }
+
+    const removeSubAttendance = (subId) => {
+        dispatch(updateStudentFields(studentID, { subId }, "RemoveStudentSubAtten"))
+            .then(() => {
+                dispatch(getUserDetails(studentID, address));
+            })
+    }
+
+    const StudentMarksSection = () => {
+        const renderTableSection = () => {
+            return (
+                <>
+                    <h3>Subject Marks:</h3>
+                    <Table>
+                        <TableHead>
+                            <StyledTableRow>
+                                <StyledTableCell>Subject</StyledTableCell>
+                                <StyledTableCell>Marks</StyledTableCell>
+                            </StyledTableRow>
+                        </TableHead>
+                        <TableBody>
+                            {subjectMarks.map((result, index) => {
+                                if (!result.subName || !result.marksObtained) {
+                                    return null;
+                                }
+                                return (
+                                    <StyledTableRow key={index}>
+                                        <StyledTableCell>{result.subName.subName}</StyledTableCell>
+                                        <StyledTableCell>{result.marksObtained}</StyledTableCell>
+                                    </StyledTableRow>
+                                );
+                            })}
+                        </TableBody>
+                    </Table>
+                    <Button variant="contained" sx={styles.styledButton} onClick={() => navigate("/Admin/students/student/marks/" + studentID)}>
+                        Add Marks
+                    </Button>
+                </>
+            )
+        }
+        const renderChartSection = () => {
+            return (
+                <>
+                    <CustomBarChart chartData={subjectMarks} dataKey="marksObtained" />
+                </>
+            )
+        }
+        return (
+            <>
+                {subjectMarks && Array.isArray(subjectMarks) && subjectMarks.length > 0
+                    ?
+                    <>
+                        {selectedSection === 'table' && renderTableSection()}
+                        {selectedSection === 'chart' && renderChartSection()}
+
+                        <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+                            <BottomNavigation value={selectedSection} onChange={handleSectionChange} showLabels>
+                                <BottomNavigationAction
+                                    label="Table"
+                                    value="table"
+                                    icon={selectedSection === 'table' ? <TableChartIcon /> : <TableChartOutlinedIcon />}
+                                />
+                                <BottomNavigationAction
+                                    label="Chart"
+                                    value="chart"
+                                    icon={selectedSection === 'chart' ? <InsertChartIcon /> : <InsertChartOutlinedIcon />}
+                                />
+                            </BottomNavigation>
+                        </Paper>
+                    </>
+                    :
+                    <Button variant="contained" sx={styles.styledButton} onClick={() => navigate("/Admin/students/student/marks/" + studentID)}>
+                        Add Marks
+                    </Button>
+                }
+            </>
+        )
+    }
+
+    const StudentDetailsSection = () => {
+        return (
+            <div>
+                Name: {userDetails.name}
+                <br />
+                Roll Number: {userDetails.rollNum}
+                <br />
+                Class: {sclassName.sclassName}
+                <br />
+                Institute: {studentSchool.schoolName}
+                <Button variant="contained" sx={styles.styledButton} onClick={deleteHandler}>
+                    Delete
+                </Button>
+            </div>
+        )
+    }
+
+    return (
+        <>
+            {loading
+                ?
+                <>
+                    <div>Loading...</div>
+                </>
+                :
+                <>
+                    <Box sx={{ width: '100%', typography: 'body1', }} >
+                        <TabContext value={value}>
+                            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                                <TabList onChange={handleChange} sx={{ position: 'fixed', width: '100%', bgcolor: 'background.paper', zIndex: 1 }}>
+                                    <Tab label="Details" value="1" />
+                                    <Tab label="Marks" value="2" />
+                                </TabList>
+                            </Box>
+                            <Container sx={{ marginTop: "3rem", marginBottom: "4rem" }}>
+                                <TabPanel value="1">
+                                    <StudentDetailsSection />
+                                </TabPanel>
+                                <TabPanel value="2">
+                                    <StudentMarksSection />
+                                </TabPanel>
+                            </Container>
+                        </TabContext>
+                    </Box>
+                </>
+            }
+            <Popup message={message} setShowPopup={setShowPopup} showPopup={showPopup} />
+        </>
+    )
+}
+
+export default ViewStudent
+
+const styles = {
+    styledButton: {
+        margin: "20px",
+        backgroundColor: "#02250b",
+        "&:hover": {
+            backgroundColor: "#106312",
+        }
+    }
+}
---
+++ frontend/src/pages/student/StudentComplain.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StudentComplain() {
+  return (
+    <div>StudentComplain</div>
+  )
+}
