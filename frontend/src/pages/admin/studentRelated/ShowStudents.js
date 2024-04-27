6f8977904d5b0930d191d644615944724370dcef
Utkarsh_Dwivedi32 <82316178+Utkarsh13-ray@users.noreply.github.com>
2024-04-27 16:09:19 +0530

    Update ShowStudents.js

--- frontend/src/pages/admin/studentRelated/ShowStudents.js
+++ frontend/src/pages/admin/studentRelated/ShowStudents.js
@@ -34,7 +34,7 @@ const ShowStudents = () => {
     const deleteHandler = (deleteID, address) => {
         console.log(deleteID);
         console.log(address);
-        setMessage("Sorry the delete function has been disabled for now.")
+        setMessage("Deleted successfully")
         setShowPopup(true)
 
         dispatch(deleteUser(deleteID, address))
